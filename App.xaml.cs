1f321903a5bd033d049b41eb5f4422fb941f9fb9
Shri Ganesh Jha <realsgj@gmail.com>
2023-07-16 11:53:25 +0000

    remove wrong imports

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.Maui;
-using Microsoft.Maui.Controls;
-using Application = Microsoft.Maui.Controls.Application;
-namespace TenkiWeather;
+﻿namespace TenkiWeather;
 
 public partial class App : Application
 {
