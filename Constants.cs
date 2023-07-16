73072e046ee1c4ac8993c0684a19e4e777e07fa0
Shri Ganesh Jha <60005430+TheSGJ@users.noreply.github.com>
2023-07-16 15:43:43 +0530

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -1,11 +1,8 @@
-namespace TenkiWeather
+namespace WeatherApp
 {
     public static class Constants
     {
-        public static string OpenWeatherMapEndpoint =
-            "https://api.openweathermap.org/data/2.5/weather";
-
-        public static string OpenWeatherMapAPIKey { get; } =
-            Environment.GetEnvironmentVariable("WEATHER_API_KEY");
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "b61233d1cb264dc5fe16286e19002b7e";
     }
 }
